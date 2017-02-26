489b3678138bc277b43adbd05e15060079003921
LIUWENCAN <pheye@qq.com>
2017-02-26 17:34:26 +0800

    1.清除没有用到的源文化和自动生成的部分CSS文件，它们导致git管理变得混乱

--- public/app/js/bigbigads.js
+++ public/app/js/bigbigads.js
@@ -256,6 +256,159 @@ app.controller('BookmarkAddController', ['$scope', 'Bookmark', 'BookmarkItem', '
     }
 }]);
 
+if (!app)
+    var app = angular.module('MetronicApp');
+app.controller('PlansController', ['$scope', 'Resource', 'User', function($scope, Resource, User) {
+    var plans = new Resource('plans');
+    plans.getPolicy = function(item, permissionKey, groupKey) {
+        var group = item.groupPermissions[groupKey], policy;
+        var i, finded = false;
+        if (!group)
+            return false;
+        angular.forEach(group, function(groupItem) {
+            if (groupItem.key == permissionKey) {
+                finded = true;   
+            }
+        });
+        if (!finded)
+            return false;
+        for(i = 0;i < item.policies.length; ++i) {
+            policy = item.policies[i];
+            if (policy.key == permissionKey) {
+                return {value:policy.pivot.value, type:policy.type};
+            }
+        }
+
+        return true;
+    };
+
+    plans.goPlanID = function(item) {
+        var id;
+        if (!item.plan)
+            return "";
+        if (plans.annually) {
+            id = item.plan.annually.id;
+        }  else  {
+            id = item.plan.monthly.id;
+        }
+        // console.log(plans.annually, id);
+        window.open("/pay?plan=" + id);
+    };
+    plans.isCurrentPlan = function(item) {
+        if (!User.info.subscription)
+            return false;
+        return User.info.subscription.braintree_plan.replace('_Monthly', '') == item.name;
+    };
+
+    plans.annually = false;
+
+    $scope.queryPromise = plans.get();
+    $scope.queryPromise.then(function(items) {
+        // console.log(items);
+        if(items.length > 0) {
+            $scope.groupPermissions = items[items.length - 1].groupPermissions;
+        }
+    });
+    $scope.plans = plans;
+    $scope.groupPermissions = [];
+    User.getInfo().then(function() {
+        $scope.userInfo = User.info;
+        if (User.info.login) {
+            $scope.subscription = User.info.subscription;
+        }
+    });
+}]);
+app.controller('ProfileController', ['$scope', '$location', 'User', '$uibModal', function($scope, $location, User, $uibModal) {
+    var profile = {
+        init:function() {
+            var search = $location.search();
+            if (search.active && search.active != this.active) {
+                this.active = Number(search.active);
+            }
+        }
+    };
+    profile.init();
+    $scope.profile = profile;
+    $scope.$watch('profile.active', function(newValue, oldValue) {
+        if (newValue == oldValue)
+            return;
+         $location.search('active', newValue);
+    });
+    $scope.$on('$locationChangeSuccess', function(ev) {
+        // console.log($location.search());
+        profile.init();
+    });
+    $scope.changePwd = function() {
+        return $uibModal.open({
+            templateUrl:'views/profile/changepwd.html',
+            size:'md',
+            animation:true,
+            controller:'ChangepwdController'
+        });
+    };
+    $scope.userPromise = User.getInfo();
+    $scope.userPromise.then(function() {
+        $scope.userInfo = User.info;
+        $scope.user = User.info.user;
+        $scope.User = User;
+    });
+}]);
+app.controller('SubscriptionController', ['$scope', 'User', function($scope, User) {
+    User.getInfo().then(function() {
+        $scope.userInfo = User.info;
+        $scope.subscription = User.info.subscription;
+    });
+}]);
+app.controller('BillingsController', ['$scope', 'User', 'Resource', function($scope, User, Resource) {
+    var billings = new Resource('billings');
+    $scope.billings = billings;
+
+    $scope.beatifyDate = function(dateStr) {
+        return dateStr.split(' ')[0];
+    };
+    User.getInfo().then(function() {
+        $scope.userInfo = User.info;
+        $scope.subscription = User.info.subscription;
+        if (!User.info.login) 
+            return;
+        $scope.queryPromise = billings.get();
+    });
+}]);
+app.controller('ChangepwdController', ['$scope', '$uibModalInstance', '$http', 'settings', function($scope, $uibModalInstance, $http, settings) {
+    var info = {
+        oldpwd:null,
+        newpwd:null,
+        repeatpwd:null
+        };
+    var url = settings.remoteurl + "/changepwd";
+    $scope.info = info;
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+    $scope.save = function(item) {
+        console.log($scope.info);
+        // $scope.promise = bookmark.save(item);
+        // $scope.promise.then(function() {
+        //     $uibModalInstance.dismiss('success');
+        // });
+       if (info.newpwd != info.repeatpwd) {
+            info.error = "repeat password is diffrent with new password";
+            return;
+       }
+       $scope.promise = $http.post(url, info);
+       $scope.promise.then(function(res) {
+            var data = res.data;
+            if (Number(data.code) !== 0) {
+                info.error = data.desc;
+                return;
+            }
+            $uibModalInstance.dismiss('save');
+       }, function(res) {
+            info.error = res.statusText;
+       });
+    };
+}]);
+
 /* common js */
 
 if (!app)
@@ -701,239 +854,6 @@ app.service('Resource', ['$resource', 'settings', 'SweetAlert', function($resour
         });
     }
     return f;
-}]);
-
-if (!app)
-    var app = angular.module('MetronicApp');
-app.controller('RankingController', ['$scope', 'settings', '$http', 'SweetAlert', '$location', function($scope, settings, $http, SweetAlert, $location) {
-    var ranking = {
-        active:0,
-        sort:{field:null, reverse:true},
-        adsers:[], 
-        ads:[], 
-        keywords:[],
-        categoryList:angular.copy(settings.searchSetting.categoryList),
-        activedTabs:[false,false,false],
-        getTopAdsers:function(category) {
-            var rankurl = settings.remoteurl + '/ranking';
-            var params = null;
-            if (category) {
-                params = {category:category};
-            }
-            console.log(params);
-
-            $http.get(rankurl, {params:params}).then(function(res) {
-                ranking.adsers = res.data;
-                angular.forEach(ranking.adsers, function(item) {
-                    if (item.page_website && !item.page_website.match("^http")) {
-                        item.page_website = "http://"  + item.page_website;
-                    }
-                });
-            }, function(res) {
-                if (res.data instanceof Object) {
-                    SweetAlert.swal(res.data.desc);
-                } else {
-                    SweetAlert.swal(res.statusText);
-                }
-            });
-        },
-        initActiveData:function() {
-            active = ranking.active;
-            if (ranking.activedTabs[active])
-                return;
-            switch(active) {
-                case 0:
-                    ranking.getTopAdsers(ranking.category);
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-            }
-            ranking.activedTabs[active] = true;
-        }
-    };
-
-    function init() {
-        var search = $location.search();
-        if (search.active)
-            ranking.active = Number(search.active);
-        if (search.category)
-            ranking.category = search.category;
-        ranking.initActiveData();
-    }
-
-    init();
-    $scope.settings = settings;
-    $scope.ranking = ranking;
-    $scope.changeRanking = function() {
-       ranking.getTopAdsers(ranking.category);
-       $location.search('category', ranking.category);
-    };
-    $scope.clear = function() {
-        ranking.category = "";
-        $scope.changeRanking();
-    };
-    $scope.$watch('ranking.active', function(newValue, oldValue) {
-        if (newValue != oldValue) {
-            $location.search("active", newValue);
-            ranking.initActiveData();
-        }
-    });
-}]);
-
-
-if (!app)
-    var app = angular.module('MetronicApp');
-app.controller('PlansController', ['$scope', 'Resource', 'User', function($scope, Resource, User) {
-    var plans = new Resource('plans');
-    plans.getPolicy = function(item, permissionKey, groupKey) {
-        var group = item.groupPermissions[groupKey], policy;
-        var i, finded = false;
-        if (!group)
-            return false;
-        angular.forEach(group, function(groupItem) {
-            if (groupItem.key == permissionKey) {
-                finded = true;   
-            }
-        });
-        if (!finded)
-            return false;
-        for(i = 0;i < item.policies.length; ++i) {
-            policy = item.policies[i];
-            if (policy.key == permissionKey) {
-                return {value:policy.pivot.value, type:policy.type};
-            }
-        }
-
-        return true;
-    };
-
-    plans.goPlanID = function(item) {
-        var id;
-        if (!item.plan)
-            return "";
-        if (plans.annually) {
-            id = item.plan.annually.id;
-        }  else  {
-            id = item.plan.monthly.id;
-        }
-        // console.log(plans.annually, id);
-        window.open("/pay?plan=" + id);
-    };
-    plans.isCurrentPlan = function(item) {
-        if (!User.info.subscription)
-            return false;
-        return User.info.subscription.braintree_plan.replace('_Monthly', '') == item.name;
-    };
-
-    plans.annually = false;
-
-    $scope.queryPromise = plans.get();
-    $scope.queryPromise.then(function(items) {
-        // console.log(items);
-        if(items.length > 0) {
-            $scope.groupPermissions = items[items.length - 1].groupPermissions;
-        }
-    });
-    $scope.plans = plans;
-    $scope.groupPermissions = [];
-    User.getInfo().then(function() {
-        $scope.userInfo = User.info;
-        if (User.info.login) {
-            $scope.subscription = User.info.subscription;
-        }
-    });
-}]);
-app.controller('ProfileController', ['$scope', '$location', 'User', '$uibModal', function($scope, $location, User, $uibModal) {
-    var profile = {
-        init:function() {
-            var search = $location.search();
-            if (search.active && search.active != this.active) {
-                this.active = Number(search.active);
-            }
-        }
-    };
-    profile.init();
-    $scope.profile = profile;
-    $scope.$watch('profile.active', function(newValue, oldValue) {
-        if (newValue == oldValue)
-            return;
-         $location.search('active', newValue);
-    });
-    $scope.$on('$locationChangeSuccess', function(ev) {
-        // console.log($location.search());
-        profile.init();
-    });
-    $scope.changePwd = function() {
-        return $uibModal.open({
-            templateUrl:'views/profile/changepwd.html',
-            size:'md',
-            animation:true,
-            controller:'ChangepwdController'
-        });
-    };
-    $scope.userPromise = User.getInfo();
-    $scope.userPromise.then(function() {
-        $scope.userInfo = User.info;
-        $scope.user = User.info.user;
-        $scope.User = User;
-    });
-}]);
-app.controller('SubscriptionController', ['$scope', 'User', function($scope, User) {
-    User.getInfo().then(function() {
-        $scope.userInfo = User.info;
-        $scope.subscription = User.info.subscription;
-    });
-}]);
-app.controller('BillingsController', ['$scope', 'User', 'Resource', function($scope, User, Resource) {
-    var billings = new Resource('billings');
-    $scope.billings = billings;
-
-    $scope.beatifyDate = function(dateStr) {
-        return dateStr.split(' ')[0];
-    };
-    User.getInfo().then(function() {
-        $scope.userInfo = User.info;
-        $scope.subscription = User.info.subscription;
-        if (!User.info.login) 
-            return;
-        $scope.queryPromise = billings.get();
-    });
-}]);
-app.controller('ChangepwdController', ['$scope', '$uibModalInstance', '$http', 'settings', function($scope, $uibModalInstance, $http, settings) {
-    var info = {
-        oldpwd:null,
-        newpwd:null,
-        repeatpwd:null
-        };
-    var url = settings.remoteurl + "/changepwd";
-    $scope.info = info;
-    $scope.cancel = function() {
-        $uibModalInstance.dismiss('cancel');
-    };
-    $scope.save = function(item) {
-        console.log($scope.info);
-        // $scope.promise = bookmark.save(item);
-        // $scope.promise.then(function() {
-        //     $uibModalInstance.dismiss('success');
-        // });
-       if (info.newpwd != info.repeatpwd) {
-            info.error = "repeat password is diffrent with new password";
-            return;
-       }
-       $scope.promise = $http.post(url, info);
-       $scope.promise.then(function(res) {
-            var data = res.data;
-            if (Number(data.code) !== 0) {
-                info.error = data.desc;
-                return;
-            }
-            $uibModalInstance.dismiss('save');
-       }, function(res) {
-            info.error = res.statusText;
-       });
-    };
 }]);
 
 if (!app)
@@ -2603,5 +2523,85 @@ app.controller('AdserSearchController', ['$rootScope', '$scope', 'settings', 'Se
 			$scope.$emit('competitor', item);
 		};
 	}]);
+
+if (!app)
+    var app = angular.module('MetronicApp');
+app.controller('RankingController', ['$scope', 'settings', '$http', 'SweetAlert', '$location', function($scope, settings, $http, SweetAlert, $location) {
+    var ranking = {
+        active:0,
+        sort:{field:null, reverse:true},
+        adsers:[], 
+        ads:[], 
+        keywords:[],
+        categoryList:angular.copy(settings.searchSetting.categoryList),
+        activedTabs:[false,false,false],
+        getTopAdsers:function(category) {
+            var rankurl = settings.remoteurl + '/ranking';
+            var params = null;
+            if (category) {
+                params = {category:category};
+            }
+            console.log(params);
+
+            $http.get(rankurl, {params:params}).then(function(res) {
+                ranking.adsers = res.data;
+                angular.forEach(ranking.adsers, function(item) {
+                    if (item.page_website && !item.page_website.match("^http")) {
+                        item.page_website = "http://"  + item.page_website;
+                    }
+                });
+            }, function(res) {
+                if (res.data instanceof Object) {
+                    SweetAlert.swal(res.data.desc);
+                } else {
+                    SweetAlert.swal(res.statusText);
+                }
+            });
+        },
+        initActiveData:function() {
+            active = ranking.active;
+            if (ranking.activedTabs[active])
+                return;
+            switch(active) {
+                case 0:
+                    ranking.getTopAdsers(ranking.category);
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+            ranking.activedTabs[active] = true;
+        }
+    };
+
+    function init() {
+        var search = $location.search();
+        if (search.active)
+            ranking.active = Number(search.active);
+        if (search.category)
+            ranking.category = search.category;
+        ranking.initActiveData();
+    }
+
+    init();
+    $scope.settings = settings;
+    $scope.ranking = ranking;
+    $scope.changeRanking = function() {
+       ranking.getTopAdsers(ranking.category);
+       $location.search('category', ranking.category);
+    };
+    $scope.clear = function() {
+        ranking.category = "";
+        $scope.changeRanking();
+    };
+    $scope.$watch('ranking.active', function(newValue, oldValue) {
+        if (newValue != oldValue) {
+            $location.search("active", newValue);
+            ranking.initActiveData();
+        }
+    });
+}]);
+
 
 //# sourceMappingURL=bigbigads.js.map
