9e0f2d8dab15c3e5291c9a77dcdae2e74f6744b1
LIUWENCAN <pheye@qq.com>
2017-03-28 14:42:10 +0800

    使用gulp进一步实现工程化配置，解决css与js的缓存问题 1.删除无用的文件，包含layout的scss文件;app/demo的后端文件;js文件 2.将index.html引用到的assets的js文件移到src/standalone目录下，实现打版本包 3.package.json将ng-highcharts固定在1.0.1版本 4.修改gulp文件，实现css在production与develop时的区别，生产环境下将最小化文件并打版本

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -12,8 +12,9 @@ var rtlcss = require("gulp-rtlcss");
 var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var clean = require('gulp-clean');
-var del = require('del');
-var runSequence = require('run-sequence');
+const del = require('del');
+const mergeStream = require('merge-stream');
+const runSequence = require('run-sequence');
 const rev = require('gulp-rev');
 const revCollector = require('gulp-rev-collector');
 const jshint = require('gulp-jshint');
@@ -38,24 +39,35 @@ gulp.task('localhost-live', function() {
 
 //*** SASS compiler task
 gulp.task('sass', function () {
-  // bootstrap compilation
-	gulp.src('./src/sass/bootstrap.scss').pipe(sass()).pipe(gulp.dest('./assets/global/plugins/bootstrap/css/'));
+    if (config.mode === "production") {
+        // var tasks = [];
+      // global theme stylesheet compilation
+      const global = gulp.src('./src/sass/global/*.scss').pipe(sass()).pipe(rev()).pipe(minifyCss()).pipe(gulp.dest('./assets/global/css')).pipe(rev.manifest('global.json')).pipe(gulp.dest('./assets/'));
+      const app = gulp.src('./src/sass/apps/*.scss').pipe(sass()).pipe(rev()).pipe(gulp.dest('./assets/apps/css')).pipe(rev.manifest('apps.json')).pipe(gulp.dest('./assets/'));
+      const pages = gulp.src('./src/sass/pages/*.scss').pipe(sass()).pipe(rev()).pipe(gulp.dest('./assets/pages/css')).pipe(rev.manifest('pages.json')).pipe(gulp.dest('./assets/'));
 
-  // select2 compilation using bootstrap variables
-	//gulp.src('./assets/global/plugins/select2/sass/select2-bootstrap.min.scss').pipe(sass({outputStyle: 'compressed'})).pipe(gulp.dest('./assets/global/plugins/select2/css/'));
+      const layout = (gulp.src('./src/sass/layouts/layout3/*.scss').pipe(sass()).pipe(rev()).pipe(gulp.dest('./assets/layouts/layout3/css')).pipe(rev.manifest('layouts-layout3.json')).pipe(gulp.dest('./assets/')));
+      const theme = gulp.src('./src/sass/layouts/layout3/themes/*.scss').pipe(sass()).pipe(rev()).pipe(gulp.dest('./assets/layouts/layout3/css/themes')).pipe(rev.manifest('layouts-layout3-theme.json')).pipe(gulp.dest('./assets/'));
+      return mergeStream(global, app, pages, layout, theme);
+    } else {
+      // bootstrap compilation
+        gulp.src('./src/sass/bootstrap.scss').pipe(sass()).pipe(gulp.dest('./assets/global/plugins/bootstrap/css/'));
 
-  // global theme stylesheet compilation
-    gulp.src('./src/sass/global/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/global/css'));
-    gulp.src('./src/sass/apps/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/apps/css'));
-    gulp.src('./src/sass/pages/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/pages/css'));
+      // select2 compilation using bootstrap variables
+        //gulp.src('./assets/global/plugins/select2/sass/select2-bootstrap.min.scss').pipe(sass({outputStyle: 'compressed'})).pipe(gulp.dest('./assets/global/plugins/select2/css/'));
 
-  // theme layouts compilation
-    gulp.src('./src/sass/layouts/layout/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout/css'));
-  gulp.src('./src/sass/layouts/layout/themes/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout/css/themes'));
+      // global theme stylesheet compilation
+      gulp.src('./src/sass/global/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/global/css'));
+      gulp.src('./src/sass/apps/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/apps/css'));
+      gulp.src('./src/sass/pages/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/pages/css'));
 
+      // theme layouts compilation
+      // gulp.src('./src/sass/layouts/layout/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout/css'));
+      // gulp.src('./src/sass/layouts/layout/themes/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout/css/themes'));
 
-  gulp.src('./src/sass/layouts/layout3/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout3/css'));
-  gulp.src('./src/sass/layouts/layout3/themes/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout3/css/themes'));
+      gulp.src('./src/sass/layouts/layout3/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout3/css'));
+      gulp.src('./src/sass/layouts/layout3/themes/*.scss').pipe(sourcemaps.init()).pipe(sass()).pipe(sourcemaps.write('./')).pipe(gulp.dest('./assets/layouts/layout3/css/themes'));
+  }
 });
 
 //*** SASS watch(realtime) compiler task
@@ -85,7 +97,7 @@ gulp.task('minify', function () {
 });
 
 gulp.task('clean', function() {
-        return gulp.src('./app/js', {read:false}).pipe(clean());
+        return gulp.src(['./app/js', './assets/*.json'], {read:false}).pipe(clean());
 })
 
 gulp.task('script',  function() {
@@ -97,12 +109,12 @@ gulp.task('script',  function() {
         gulp.src(['./src/index.html']).pipe(gulp.dest('./app/'));
     } else {
         gulp.src(['./src/js/**/*.js', '!./src/js/standalone/**/*.js']).pipe(concat(target)).pipe(gulp.dest('./app/js/')).pipe(uglify()).pipe(gulp.dest('./app/js/'));
-        gulp.src(['./src/js/standalone/**/*.js'])
+        return gulp.src(['./src/js/standalone/**/*.js'])
             .pipe(uglify())
             .pipe(rev())
             .pipe(gulp.dest(config.script.target))
             .pipe(rev.manifest())
-            .pipe(gulp.dest('./app'));
+            .pipe(gulp.dest('./assets'));
 
     }
     // gulp.src(['./app/js/' + target]).pipe(uglify()).pipe(rename({suffix:'.min'})).pipe(gulp.dest('./app/js/'));
@@ -132,11 +144,12 @@ gulp.task('lint', function()  {
 
 //对JS与CSS生成版本号，防止缓存;源文件必须与目标文件分开，否则将只能替换一次
 gulp.task('rev',  function() {
-    return gulp.src(['./app/*.json', './src/index.html']).pipe(revCollector({
+    return gulp.src(['./assets/*.json',  './src/index.html']).pipe(revCollector({
                 replaceReved:true
                 }))
                 .pipe(gulp.dest('./app'));
 });
+
 
 gulp.task('script:watch', function() {
     gulp.watch(['./src/js/**/*.js'], ['lint', 'script']);
@@ -144,8 +157,8 @@ gulp.task('script:watch', function() {
 
 gulp.task('watch', ['sass:watch', 'script:watch']);
 
-gulp.task('production', ['clean'], function(cb) {
+gulp.task('config-product', function() {
     config.mode = "production";
-    return runSequence(['sass'], ['script'], ['minify'], ['rev'],  cb);
-});
+})
+gulp.task('production', gulpsync.sync([["config-product", 'clean'], ['sass', 'script'], 'rev']));
 gulp.task('develop', ['sass', 'script']);
