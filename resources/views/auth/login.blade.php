5bbb993215ba3f5a72a7b4aac8fe1ba3f939ec53
LIUWENCAN <pheye@qq.com>
2017-06-22 22:24:47 +0800

    社交登陆注册的帐号，也支持联盟会员“

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -106,9 +106,9 @@
                 <div class="form-group text-center">
                     <!-- 暂时注释社交登录部分 -->
                     <!-- <a href="/socialite/github" class="github"><i class="fa fa-github fa-3x"></i></a> -->
-                    <a href="/socialite/facebook" class="facebook"><i class="fa fa-facebook-official fa-3x"></i></a>
-                    <a href="/socialite/linkedin" class="linkedin"><i class="fa fa-linkedin-square fa-3x"></i></a>
-                    <a href="/socialite/google" class="google-plus"><i class="fa fa-google-plus-square fa-3x"></i></a>
+                    <a href="/socialite/facebook" class="facebook socialite"><i class="fa fa-facebook-official fa-3x"></i></a>
+                    <a href="/socialite/linkedin" class="linkedin socialite"><i class="fa fa-linkedin-square fa-3x"></i></a>
+                    <a href="/socialite/google" class="google-plus socialite"><i class="fa fa-google-plus-square fa-3x"></i></a>
                 </div>
                 <div class="login-footer row">
                     <!--create an acount-->
@@ -308,7 +308,7 @@
         <script src="./assets/global/plugins/jquery-validation/js/additional-methods.min.js" type="text/javascript"></script>
         <!-- END PAGE LEVEL PLUGINS -->
         <!-- BEGIN PAGE LEVEL SCRIPTS -->
-        <script src="./assets/pages/scripts/login.js?v=2" type="text/javascript"></script>
+        <script src="./assets/pages/scripts/login.js?v=3" type="text/javascript"></script>
         <!-- END PAGE LEVEL SCRIPTS -->
         <!-- BEGIN THEME LAYOUT SCRIPTS -->
         <!-- END THEME LAYOUT SCRIPTS -->
