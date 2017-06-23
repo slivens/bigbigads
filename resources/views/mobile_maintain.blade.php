2471fed99dfa6f4962b93737b52a0fd9cf46e6e2
yuqinghong <niming175@qq.com>
2017-06-23 16:37:44 +0800

    删除了多余插件的引用，修改了高度适应

--- resources/views/mobile_maintain.blade.php
+++ resources/views/mobile_maintain.blade.php
@@ -1,88 +1,86 @@
 <!DOCTYPE html>
-
-
 <html lang="en">
 <!--<![endif]-->
 <!-- BEGIN HEAD -->
 <title>Updating...</title>
-
 <head>
     <meta charset="utf-8" />
-    <title>Metronic Admin Theme #5 | 500 Page Option 2</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
-    <meta content="Preview page of Metronic Admin Theme #5 for 500 page option 2" name="description" />
-    <meta content="" name="author" />
+    <meta content="The bigbigads.com's phone interface is being maintained" name="description" />
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
     <link href="http://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="http://cdn.bootcss.com/bootstrap/3.3.6//css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
     <!-- END GLOBAL MANDATORY STYLES -->
     <link rel="shortcut icon" type="image/x-icon" href="static/images/favicon.ico" media="screen">
     <style type="text/css">
-    /*升级临时页面*/
-    html {
-        font-size: calc(100vw / 10);
-        height: 100%
-    }
-    body {
-        background-color: #f5f8fa;
-        font-size: 16px;
-        height: 100%;
-    }
-    .content {
-        width: 100%;
-        height: 100%;
-        flex-direction: column;
-    }
-    
-    .navbar-header {
-        padding: 0.4rem;
-    }
-    
-    .navbar-static-top {
-        border-bottom: 1px solid #d3e0e9;
-        background: #fff;
-        margin-bottom: 0;
-    }
-    
-    .navbar-static-top .navbar-brand {
-        padding: 0px;
-    }
-    .update_page-content {
-        margin: 0 auto;
-        /* background: url(http://image1.bigbigads.com:88/image/upgrade/update_page02.jpg) no-repeat;*/
-        background: url(static/images/update_page02.jpg) no-repeat center;
-        background-size: auto 114%;
-        padding: 3.126666rem 0.6rem;
-        flex-grow: 0;
-    }
-    
-    .update_page-content .content-text {
-        overflow: hidden;
-        color: #fff;
-        width: 60%;
-    }
-    
-    .update_page-content .content-text p {
-        color: rgba(255, 255, 255, 0.7);
-        font-family: "open sans";
-        font-size: 0.5333333rem;
-        font-weight: 200;
-    }
-    
-    .update_page-foot {
-        background: #333;
-        color: #fff;
-        width: 100%;
-        padding: 0.4rem;
-    }
-    
-    .update_page-foot p {
-        font-weight: 200;
-        font-size: 0.42666rem;
-        margin-bottom: 0;
-    }
+        /*移动端维护页面*/
+        html {
+            font-size: calc(100vw / 10);
+            height: 100%
+        }
+        body {
+            background-color: #f5f8fa;
+            font-size: 16px;
+            height: 100%;
+            margin: 0;
+        }
+        .content {
+            width: 100%;
+            height: 100%;
+        }
+        
+        .navbar-header {
+            padding: 0.4rem;
+        }
+        .navbar-static-top {
+            border-bottom: 1px solid #d3e0e9;
+            background: #fff;
+            margin-bottom: 0;
+        }
+        .navbar-static-top .navbar-brand {
+            padding: 0px;
+            height: auto;
+        }
+        .navbar-static-top .navbar-brand img{
+            height: 1.226rem;
+        }
+        .update_page-content {
+            margin: 0 auto;
+            background: url(static/images/update_page02.jpg) no-repeat center;
+            background-size: auto 114%;
+            height: calc(100% - (0.4rem) * 4 - 1.266rem - 0.64rem);
+            
+        }
+        .update_page-content .content-text {
+            overflow: hidden;
+            color: #fff;
+            width: 60%;
+            display: flex;
+            justify-content: center; 
+            align-items: center;
+            height: 100%;
+            padding-left:0.4rem;
+        }
+        .update_page-content .content-text p {
+            color: rgba(255, 255, 255, 0.7);
+            font-family: "open sans";
+            font-size: 0.5333333rem;
+            font-weight: 200;
+        }
+        .update_page-foot {
+            background: #333;
+            color: #fff;
+            padding: 0.4rem;
+        }
+        .update_page-foot p {
+            font-weight: 200;
+            font-size: 0.42666rem;
+            margin: 0;
+            text-align: center;
+            height:0.64rem;
+            line-height: 0.64rem; 
+        }
     </style>
 </head>
 <!-- END HEAD -->
