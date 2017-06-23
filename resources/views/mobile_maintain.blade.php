a856a301459a9c3c75889233d5e148a6ac00dd19
yuqinghong <niming175@qq.com>
2017-06-23 10:38:34 +0800

    添加了手机维护时的临时页面

---
+++ resources/views/mobile_maintain.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+<title>Updating...</title>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Metronic Admin Theme #5 | 500 Page Option 2</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="Preview page of Metronic Admin Theme #5 for 500 page option 2" name="description" />
+    <meta content="" name="author" />
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="http://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="http://cdn.bootcss.com/bootstrap/3.3.6//css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <link rel="shortcut icon" type="image/x-icon" href="static/images/favicon.ico" media="screen">
+    <style type="text/css">
+    /*升级临时页面*/
+    html {
+        font-size: calc(100vw / 10);
+        height: 100%
+    }
+    body {
+        background-color: #f5f8fa;
+        font-size: 16px;
+        height: 100%;
+    }
+    .content {
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+    }
+    
+    .navbar-header {
+        padding: 0.4rem;
+    }
+    
+    .navbar-static-top {
+        border-bottom: 1px solid #d3e0e9;
+        background: #fff;
+        margin-bottom: 0;
+    }
+    
+    .navbar-static-top .navbar-brand {
+        padding: 0px;
+    }
+    .update_page-content {
+        margin: 0 auto;
+        /* background: url(http://image1.bigbigads.com:88/image/upgrade/update_page02.jpg) no-repeat;*/
+        background: url(static/images/update_page02.jpg) no-repeat center;
+        background-size: auto 114%;
+        padding: 3.126666rem 0.6rem;
+        flex-grow: 0;
+    }
+    
+    .update_page-content .content-text {
+        overflow: hidden;
+        color: #fff;
+        width: 60%;
+    }
+    
+    .update_page-content .content-text p {
+        color: rgba(255, 255, 255, 0.7);
+        font-family: "open sans";
+        font-size: 0.5333333rem;
+        font-weight: 200;
+    }
+    
+    .update_page-foot {
+        background: #333;
+        color: #fff;
+        width: 100%;
+        padding: 0.4rem;
+    }
+    
+    .update_page-foot p {
+        font-weight: 200;
+        font-size: 0.42666rem;
+        margin-bottom: 0;
+    }
+    </style>
+</head>
+<!-- END HEAD -->
+<body class=" page-500-full-page">
+    <div class="content">
+        <div class="update-page-head clearfix">
+            <nav class="navbar navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <!-- Branding Image -->
+                        <a class="navbar-brand" href="/">
+                            <!-- Bigbigads -->
+                            <!-- set ng filter images request is no work, so request image from remote org  -->
+                            <img src="http://image1.bigbigads.com:88/image/upgrade/logo2.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="update_page-content clearfix">
+            <div class="content-text">
+                <p class="text-center">We are redesigning the interface for our mobile app so you may find it's not available now.Please login our service using the desktop device.We are sorry for the inconvenience.
+                </p>
+            </div>
+        </div>
+        <div class="update_page-foot clearfix">
+            <p class="text-center">&copy;2017 BIGBIGADS.COM</p>
+        </div>
+    </div>
+</body>
+</html>
