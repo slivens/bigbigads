537645828aa102032f1d689aa8102bff5d71f248
rmz <734452449@qq.com>
2017-06-24 20:32:19 +0800

    导入正确的animate.css库

--- resources/assets/js/mobile.js
+++ resources/assets/js/mobile.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import './../sass/demo.scss';
 import './../sass/mobile.scss';
-import 'animate.min.css/animate.min.css';
+import 'animate.css/animate.css';
 /*$(document).ready(function(){
 	changeWord(0)
 	$('a[href="#ads-register"]').on("click",linkToUp)
