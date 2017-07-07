e800e39bb0cd83743d52c943ea886837ef4dff36
LIUWENCAN <pheye@qq.com>
2017-07-07 22:31:58 +0800

    解决广告搜索页多发一次请求的问题

--- public/src/js/search/search.js
+++ public/src/js/search/search.js
@@ -948,7 +948,7 @@ app.controller('AdsearchController', ['$rootScope', '$scope', 'settings', 'Searc
 			//一切的操作应该是在获取到用户信息之后，后面应该优化直接从本地缓存读取
 			User.getInfo().then(function() {
 				//根据search参数页面初始化
-				$scope.search('search');
+				// $scope.search('search');
 			});
 			$scope.$on('$viewContentLoaded', function() {
 				// initialize core components
