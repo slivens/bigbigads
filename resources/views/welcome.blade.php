7e39f7d4a3ba6c209648f0e82d2d49323ad0bc8e
rmz <734452449@qq.com>
2017-07-17 19:24:34 +0800

    补上操作失误漏了ActionLogin常量类的处理，规范化welcome页面引入谷歌统计代码

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -145,14 +145,8 @@
 </style>
 <!-- 根据谷歌Tag Assistant插件提示做修改 -->
 <!-- 谷歌统计代码 -->
+@include('tpl.script')
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-87299802-3', 'auto');
-  ga('send', 'pageview');
-
   /*
     for example
     ga('send', {
