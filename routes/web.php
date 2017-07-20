e5b844b62bd63b1ad3229856e1c62170ebd5c59a
XuDong <i@xudong.cc>
2017-07-20 10:21:29 +0800

    删除不该现在提交的代码

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ use TCG\Voyager\Models\Setting;
 // 前台主页
 Route::get('/', 'HomeController@index')->middleware('track');
 Route::get('/home', 'HomeController@index')->middleware('track');
+
+// 用于前台主页动态获取广告总数
 Route::get('/home/get_total_count', 'HomeController@getTotalCount');
 
 Route::get('/message', 'Controller@messageView');
@@ -171,6 +173,3 @@ Route::get('/wordpress/track_notice', 'WordpressController@trackNotice')->middle
 Route::get('/welcome', function () {
     return view('welcome');
 });
-
-// 获取发送邮件模版
-Route::get('/generate_mail_content', 'GenerateMailContentController@index');
