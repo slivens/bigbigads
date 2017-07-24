51439ec710813e479278d04e2280f3a67c943e24
yuqinghong <niming175@qq.com>
2017-07-24 10:32:00 +0800

    修改了一些错误

--- resources/views/tpl/header.blade.php
+++ resources/views/tpl/header.blade.php
@@ -6,7 +6,7 @@
         <div class="pull-left">
             <a class="logo-head navbar-brand" href="/home" title=""></a>
         </div>
-        <div class=" pull-right menu_list">
+        <div class=" pull-right menu-list">
             <a href="{{url('/login')}}" class="btn btn-empty w100">LOGIN</a>
             <a href="{{url('/register')}}" class="btn w100 head-btn">SIGN UP</a>
             <!--应使用register路由，否则在登陆后点注册会因找不到login#register出错-->
