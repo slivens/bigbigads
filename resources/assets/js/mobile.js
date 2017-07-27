1bba80b3e30e33acdad4c62c6f05c2424b6cf7fd
yuqinghong <niming175@qq.com>
2017-07-27 19:49:20 +0800

    修改、恢复 使代码能通过esline

--- resources/assets/js/mobile.js
+++ resources/assets/js/mobile.js
@@ -1,81 +1,74 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './../sass/demo.scss';
-import './../sass/mobile.scss';
-import 'animate.css/animate.css';
-import 'font-awesome/css/font-awesome.min.css';
-/*$(document).ready(function(){
-    changeWord(0)
-    $('a[href="#ads-register"]').on("click",linkToUp)
-})*/
+import 'bootstrap/dist/css/bootstrap.css'
+import './../sass/demo.scss'
+import './../sass/mobile.scss'
+import 'animate.css/animate.css'
+import 'font-awesome/css/font-awesome.min.css'
 (function() {
     changeWord(0)
-    getAdsCount() //获取广告数和广告主数量
+    getAdsCount() // 获取广告数和广告主数量
     $('a[href="#ads-register"]').on("click", linkToUp)
 
-    /*动态换词*/
+    /* 动态换词 */
     function changeWord(item) {
-        var word = ["Ad Creatives", " Audience Targeting", "Ad Run Time", "Tracking Tool", "Eshop Platform"];
+        var word = ["Ad Creatives", " Audience Targeting", "Ad Run Time", "Tracking Tool", "Eshop Platform"]
         if (word.length <= item) {
-            item = 0;
+            item = 0
         }
-        $("#changeWord").html(word[item]);
-        $("#changeWord").addClass("fadeIn");
+        $("#changeWord").html(word[item])
+        $("#changeWord").addClass("fadeIn")
         setTimeout(function() {
-            $("#changeWord").removeClass("fadeIn");
+            $("#changeWord").removeClass("fadeIn")
             setTimeout(function() {
-                changeWord(item + 1);
-            }, 500);
-        }, 1500);
-
+                changeWord(item + 1)
+            }, 500)
+        }, 1500)
     }
 
-
-    /*页面内锚点连接上滑*/
+    /* 页面内锚点连接上滑 */
     function linkToUp() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var $target = $(this.hash);
-            $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+            var $target = $(this.hash)
+            $target = ($target.length && $target) || $('[name=' + this.hash.slice(1) + ']')
             if ($target.length) {
-                var targetOffset = $target.offset().top;
+                var targetOffset = $target.offset().top
                 $('html,body').animate({
-                        scrollTop: targetOffset
-                    },
-                    300);
-                return false;
+                    scrollTop: targetOffset
+                },
+                300)
+                return false
             }
         }
     }
 
-     /*异步获取广告数*/
+    /* 异步获取广告数 */
     function getAdsCount() {
-
-        /*采用fetch获取数据*/
-        //需要跨域请求，有待解决
+        /* 采用fetch获取数据 */
+        // 需要跨域请求，有待解决
         var data = {
-            method: "get",
+            method: "get"
         }
         fetch('/get_total_count', data).then(function(response) {
-            return response.json();
+            return response.json()
         }).then(function(json) {
             try {
-                var adscount = estimationThousand(json.total_ads_count);
-                var adsercount = estimationThousand(json.total_adser_count);
-                $("#adsnumber").html(adscount + "<sup>+</sup>");
-                $("#adsernumber").html(adsercount + "<sup>+</sup>");
+                var adscount = estimationThousand(json.total_ads_count)
+                var adsercount = estimationThousand(json.total_adser_count)
+                $("#adsnumber").html(adscount + "<sup>+</sup>")
+                $("#adsernumber").html(adsercount + "<sup>+</sup>")
             } catch (e) {
-                $("#adsnumber").html("5,300,000" + "<sup>+</sup>");
-                $("#adsernumber").html("1,300,000" + "<sup>+</sup>");
+                $("#adsnumber").html("5,300,000" + "<sup>+</sup>")
+                $("#adsernumber").html("1,300,000" + "<sup>+</sup>")
             }
         }).catch(function(ex) {
-            $("#adsnumber").html("5,300,000" + "<sup>+</sup>");
-            $("#adsernumber").html("1,300,000" + "<sup>+</sup>");
-        });
+            $("#adsnumber").html("5,300,000" + "<sup>+</sup>")
+            $("#adsernumber").html("1,300,000" + "<sup>+</sup>")
+        })
     }
-    //估算到千位
-    function estimationThousand(num){
-        var count = parseInt(Math.round(num / 1000));
-        count = count.toString().replace(/(^|\s)\d+/g, (m) => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','));
-        count = count + ",000";
-        return count;
+    // 估算到千位
+    function estimationThousand(num) {
+        var count = parseInt(Math.round(num / 1000))
+        count = count.toString().replace(/(^|\s)\d+/g, (m) => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','))
+        count = count + ",000"
+        return count
     }
-})();
+})()
--- resources/assets/js/pay.js
+++ resources/assets/js/pay.js
@@ -1,198 +1,195 @@
 import Vue from 'vue'
 import axios from 'axios'
 import moment from 'moment'
-import { SweetModal } from 'sweet-modal-vue'
-import 'bootstrap/dist/css/bootstrap.css';
-import './../sass/demo.scss';
-import './../sass/pay.scss';
-import 'font-awesome/css/font-awesome.min.css';
-// Vue.component('coupon', require('./components/Coupon.vue'));
-/*判断邮箱*/
+import {
+    SweetModal
+} from 'sweet-modal-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import './../sass/demo.scss'
+import './../sass/pay.scss'
+import 'font-awesome/css/font-awesome.min.css'
+// Vue.component('coupon', require('./components/Coupon.vue'))
 
+/* 判断邮箱 */
 function isEmail(szMail) {
-    var szReg = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
-    var bChk = szReg.test(szMail);
-    return bChk;
+    var szReg = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/
+    var bChk = szReg.test(szMail)
+    return bChk
 }
 
-const app = new Vue({
+/* eslint-disable no-new */
+new Vue({
     el: '#app',
     data: {
-        couponObject:null,
-        coupon:'', 
-        loading:false,
-        amount:0,
-        discount:0,
-        inited:true,
-        errorMessage:"",
-        email:'',
-        emailErr:false,
-        emailMessage:"Required",
-        ipAddr:"0.0.0.0",
-        showLoading:true,//showLoding 控制的是hidden的样式，当为true的时候，为隐藏！
+        couponObject: null,
+        coupon: '',
+        loading: false,
+        amount: 0,
+        discount: 0,
+        inited: true,
+        errorMessage: "",
+        email: '',
+        emailErr: false,
+        emailMessage: "Required",
+        ipAddr: "0.0.0.0",
+        showLoading: true // showLoding 控制的是hidden的样式，当为true的时候，为隐藏！
     },
     components: {
         SweetModal
     },
     methods: {
         applyCoupon: function() {
-            const that = this;
-            that.loading = true;
+            const that = this
+            that.loading = true
             axios.get(`/rest/coupon?where={"code":"${this.coupon}"}`)
                 .then(response => {
-                    that.loading = false;
+                    that.loading = false
                     if (response.status === 200 && response.data.length > 0) {
-                        that.couponObject = response.data[0];
+                        that.couponObject = response.data[0]
                         if (!that.checkDiscount()) {
-                            that.couponObject = null;
-                            that.coupon = "";
-                            return;
+                            that.couponObject = null
+                            that.coupon = ""
+                            return
                         }
-                        that.discount = that.getDiscount();
+                        that.discount = that.getDiscount()
                     } else {
-                        that.couponObject = null;
-                        that.discount = 0;
-                        that.coupon = "";
-                        that.errorMessage = "The coupon is not found";
-                        that.$refs.modal.open();
+                        that.couponObject = null
+                        that.discount = 0
+                        that.coupon = ""
+                        that.errorMessage = "The coupon is not found"
+                        that.$refs.modal.open()
                     }
-                        
-                    // console.log(response);
-                }).catch(error => {
-                    // console.log(error);
-                    that.loading = false;
-                    that.couponObject = null;
-                });
+
+                    // console.log(response)
+                }).catch(() => {
+                    // console.log(error)
+                    that.loading = false
+                    that.couponObject = null
+                })
         },
-        toRegister: function(){
-            const that = this;
-            var userEmail = that.email;
-            var userName = userEmail.split("@")[0];
-            var regTrack = "";
-            var track = JSON.parse(window.localStorage.getItem('track')); //获取联盟会员的track码
-            if(track){
+        toRegister: function() {
+            const that = this
+            var userEmail = that.email
+            var userName = userEmail.split("@")[0]
+            var regTrack = ""
+            var track = JSON.parse(window.localStorage.getItem('track')) // 获取联盟会员的track码
+            if (track) {
                 if (Date.parse(new Date()) < Date.parse(track.expired)) {
-                    regTrack = track.code;
+                    regTrack = track.code
                 }
             }
-            that.showLoading = false;
-            if(isEmail(userEmail)){
-                //如果符合需求，进行注册
-                axios.post('/quick_register',{'email':userEmail,'password':userEmail, 'name':userName, 'track': regTrack}).then(
-                    response =>{
-                        if(response.status === 200){
-                            //该邮箱已经被注册
-                            if(response.data.code === -1){
-                                that.showLoading = true;
-                                that.emailErr = true;
-                                //that.emailMessage = response.data.desc.email[0];
-                                that.emailMessage =  "Already Registered";
+            that.showLoading = false
+            if (isEmail(userEmail)) {
+                // 如果符合需求，进行注册
+                axios.post('/quick_register', {
+                    'email': userEmail,
+                    'password': userEmail,
+                    'name': userName,
+                    'track': regTrack
+                }).then(
+                    response => {
+                        if (response.status === 200) {
+                            // 该邮箱已经被注册
+                            if (response.data.code === -1) {
+                                that.showLoading = true
+                                that.emailErr = true
+                                // that.emailMessage = response.data.desc.email[0]
+                                that.emailMessage = "Already Registered"
+                            } else if (response.data.code === 0) {
+                                // 注册成功，提交表单
+                                that.toCheckout()
+                            } else {
+                                that.showLoading = true
+                                that.errorMessage = "There was an error, please check it"
+                                that.$refs.modal.open()
                             }
-                            else if(response.data.code === 0)
-                            {
-                                //注册成功，提交表单
-                                toCheckout();
-                            }
-                            else{
-                                that.showLoading = true;
-                                that.errorMessage = "There was an error, please check it!";
-                                that.$refs.modal.open();
-                                that.email = "";
-                            }
+                        } else {
+                            that.showLoading = true
+                            that.errorMessage = "There was an error, please try again later!"
+                            that.$refs.modal.open()
+                            that.email = ""
                         }
-                        else
-                        {
-                            that.showLoading = true;
-                            that.errorMessage = "There was an error, please try again later!";
-                            that.$refs.modal.open();
-                            that.email = "";
-                        }
-                    }).catch(error => {
-                    // console.log(error);
-                    that.showLoading = true;
-                    that.errorMessage = "There was an error, please try again later!";
-                    that.$refs.modal.open();
-                    that.email = "";
-                });
-            }
-            else{
-                that.showLoading = true;
-                that.errorMessage = "Invalid";
-                that.$refs.modal.open();
+                    }).catch(() => {
+                    // console.log(error)
+                    that.showLoading = true
+                    that.errorMessage = "There was an error, please try again later!"
+                    that.$refs.modal.open()
+                    that.email = ""
+                })
+            } else {
+                that.showLoading = true
+                that.errorMessage = "Invalid"
+                that.$refs.modal.open()
             }
         },
-        //输入事件，判断邮箱格式
-        onEmailEnter: function(){
-            const that = this;
-            var userEmail = that.email;
-            if(!isEmail(userEmail)){
-                that.emailErr=true;
-                that.emailMessage="Invalid"
-                return false;
-            }
-            else{
-                that.emailErr=false;
-                return true;
+        // 输入事件，判断邮箱格式
+        onEmailEnter: function() {
+            const that = this
+            var userEmail = that.email
+            if (!isEmail(userEmail)) {
+                that.emailErr = true
+                that.emailMessage = "Invalid"
+                return false
+            } else {
+                that.emailErr = false
+                return true
             }
         },
-        //支付按钮点击
-        toCheckout: function(){
-            this.showLoading = false;
-            this.$refs.checkout.submit();
+        // 支付按钮点击
+        toCheckout: function() {
+            this.showLoading = false
+            this.$refs.checkout.submit()
         },
         initAmount: function(amount) {
             this.amount = amount
         },
         checkDiscount: function() {
-            let obj = this.couponObject;
+            let obj = this.couponObject
             if (obj.status === 0) {
-                this.errorMessage = "The coupon is invalid";
-                this.$refs.modal.open();
-                return false;
+                this.errorMessage = "The coupon is invalid"
+                this.$refs.modal.open()
+                return false
             }
             if (this.amount < obj.total) {
-                this.errorMessage = `Your order price should be more than ${obj.total}`;
-                this.$refs.modal.open();
-                return false;
+                this.errorMessage = `Your order price should be more than ${obj.total}`
+                this.$refs.modal.open()
+                return false
             }
             if (obj.used >= obj.uses) {
-                this.errorMessage = `The coupon is exhauted`;
-                this.$refs.modal.open();
-                return false;
+                this.errorMessage = `The coupon is exhauted`
+                this.$refs.modal.open()
+                return false
             }
             if (!obj.start || !obj.end) {
-                this.errorMessage = `The coupon is invalid`;
-                this.$refs.modal.open();
-                return false;
+                this.errorMessage = `The coupon is invalid`
+                this.$refs.modal.open()
+                return false
             }
             if (moment().isBefore(obj.start) || moment().isAfter(obj.end)) {
-                this.errorMessage = `The coupon is expired`;
-                this.$refs.modal.open();
-                return false;
+                this.errorMessage = `The coupon is expired`
+                this.$refs.modal.open()
+                return false
             }
-            return true;
+            return true
         },
         getDiscount: function() {
-            let obj = this.couponObject;
-            let amount = this.amount;
+            let obj = this.couponObject
+            let amount = this.amount
             if (!obj)
-                return 0;
+                return 0
             if (obj.type === 0 && obj.discount <= 100)
-                return Math.floor(amount * obj.discount / 100);
+                return Math.floor(amount * obj.discount / 100)
             if (obj.type === 1 && amount >= obj.discount)
-                return obj.discount;
-            return 0;
+                return obj.discount
+            return 0
         }
     },
-    mounted () {
-        try{
-            this.ipAddr = returnCitySN.cip;
+    mounted() {
+        try {
+            /* eslint-disable no-undef */
+            this.ipAddr = returnCitySN.cip
+        } catch (e) {
+            this.ipAddr = '0.0.0.0'
         }
-        catch(e){
-            this.ipAddr = '0.0.0.0';
-        }
-        
     }
 })
-
-
