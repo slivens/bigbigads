5997274d8f4145561bcd2f2b54f9cf46ab7492b6
LIUWENCAN <pheye@qq.com>
2017-08-08 19:53:17 +0800

    删除home.blade.php;解决safari下/home提示无fetch的问题

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 
     <title>Bigbigads Home | The Largest  Facebook Ad Examples to Smart Your Facebook Marketing</title>
     <link rel="shortcut icon" type="image/x-icon" href="./static/images/favicon.ico" media="screen" /> 
-    <link href="./dist/home.css?v=2.0.1" rel="stylesheet">
+    <link href="./dist/home.css?v=2.0.2" rel="stylesheet">
     <style type="text/css">
 
     </style>
@@ -305,8 +305,8 @@ src="https://www.facebook.com/tr?id=1555915891116409&ev=PageView&noscript=1"
         </div>
     </div>
 </div>
-<script type="text/javascript" src="dist/vendor.js?v=5.1" defer></script>
-<script type="text/javascript" src="dist/home.js?v=2.0.1" defer></script>
+<script type="text/javascript" src="dist/vendor.js?v=5.2" defer></script>
+<script type="text/javascript" src="dist/home.js?v=2.0.0" defer></script>
 </body>
 </html>
     
