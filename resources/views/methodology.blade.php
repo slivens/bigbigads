5affc383c467607a5b69211a6fc3879992f4e720
yuqinghong <niming175@qq.com>
2017-08-24 21:10:19 +0800

    添加methodology页面

---
+++ resources/views/methodology.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bigbigads Methodology | Largest Facebook Ad Examples</title>
+    <meta name="description" content="A Facebook ads spy tool to help you find out the competitor marketing strategy, ad creatives, ad insight.">
+    <link rel="shortcut icon" type="image/x-icon" href="./static/images/favicon.ico" media="screen" /> 
+    <link href="./dist/home.css?v=2.0.5" rel="stylesheet">
+    @include('tpl.script')
+</head>
+<body id="methodology">
+@include('tpl.header')
+<section class="container">
+    <p class="ads-font-30 text-center meth-title text-center ads-weight-bold">BIGBIGADS METHODOLOGY</p>
+    <div class="meth-content ads-font-18">
+        <p>BIGBIGADS doesn’t rely on any single channel for data collection.</p>
+        <p>We work with various sources to create the most accurate and reliable picture of the digital ad world.</p>
+        <p>All of the data is fed into BIGBIGADS’ data processing servers where we turn tens of millions of daily data into insightful information.</p>
+    </div>
+    <div class="meth-list ads-font-16">
+        <p>Our data comes from 4 main sources:</p>
+        <ul>
+            <li>1) Directly permission or share from the Facebook advertiser.</li>
+            <li>2) Anonymous processed big data from local ad agency all over the world.</li>
+            <li>3) Anonymous processed big data from 3rd party partners.</li>
+            <li>4) Hundreds of thousands of direct measurement sources from websites and apps that are connected to us directly. This last source of data helps us to constantly improve our learning model, fine tune our algorithms and reach accurate estimations about Facebook ad performance for ALL websites and mobile apps.</li>
+        </ul>
+    </div>
+    <div class="meth-bottom ads-font-16">
+        <p>BIGBIGADS spent several years building the data collection infrastructure and refining the data collection processes before launching. We are confident that we offer the most accurate and unbiased data covering the digital world.</p>
+        <p><span>Contact us:</span> <a href="mailto:sale@bigbigads.com">help@bigbigads.com</a></p>
+    </div>
+</section>
+@include('tpl.footer')
+</body>
+</html>
+
