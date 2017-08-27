fd56124f40bf20da92e424bd8297f7d9c81a5586
LIUWENCAN <pheye@qq.com>
2017-08-27 11:57:51 +0800

    导出Voyager后台配置;权限需要手动生成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(MenuItemsTableSeeder::class);
         $this->call(DataTypesTableSeeder::class);
         $this->call(DataRowsTableSeeder::class);
-        $this->call(SettingsTableSeeder::class);
+        $this->call(MenusTableSeeder::class);
+        $this->call(MenuItemsTableSeeder::class);
+        /* $this->call(SettingsTableSeeder::class); */
     }
 }
