69875e5c90e3edd6643393e2d3546e141efa14b0
yuqinghong <niming175@qq.com>
2017-10-09 10:24:47 +0800

    弹窗注册：去掉谷歌自动填充，name为空时自动填充；广告的linkbutton样式细节调整

--- public/src/components/register.js
+++ public/src/components/register.js
@@ -3,7 +3,7 @@ import './register.scss'
 
 angular.module('MetronicApp').controller('RegisterController', ['$scope', '$http', '$window', 'User', function($scope, $http, $window, User) {
     $scope.User = User
-    $scope.formData = {name: ' ', email: '', password: ''}
+    $scope.formData = {email: '', name: '', password: ''}
     $scope.registerError = {}
     $scope.isShow = false
     $scope.logout = function() {
@@ -19,6 +19,15 @@ angular.module('MetronicApp').controller('RegisterController', ['$scope', '$http
                 $scope.showHotmailMessage = true
             }
         })
+    }
+    /*
+    * 默认的用户名
+    * 当用户名为空时，会自动填充为邮箱的前半段
+    */
+    $scope.defaultName = function() {
+        if ($scope.formData.email && !$scope.formData.name) {
+            $scope.formData.name = $scope.formData.email.split('@')[0]
+        }
     }
     $scope.processForm = function(isValid) {
         $scope.isShow = true
