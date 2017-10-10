9059f5f75955538699157ebe977c22dd9b237cfd
solomon chen <g@solomon-2.local>
2017-10-10 16:56:57 +0800

    过期用户在修复之前，增加paypal同步

--- app/Console/Commands/ScanUsers.php
+++ app/Console/Commands/ScanUsers.php
@@ -60,15 +60,30 @@ class ScanUsers extends Command
             $this->comment("only scan user: $email ". $user->isExpired());
         } else {
             $this->comment("start scan users...");
+            //modify by chenxin 20171010
+            //因为webhook不稳定，某些用户的订阅和交易订单没同步下来，这时候users表的expired字段是不准确。
+            //如果简单粗暴地只是根据users表的expired字段来判断是否过期，就容易出现误判。
+            //本次改造点：针对过期的用户，多执行一下同步订阅和订单，保证跟paypal远端一致后，再执行原来的业务逻辑
+            $paymentService = app(\App\Contracts\PaymentService::class);
+            $paymentService->setParameter(PaymentService::PARAMETER_FORCE, true);
+            $paymentService->setLogger($this);
+
             foreach (User::where('role_id', '>', 2)->cursor() as $user) {
                 if ($fix) {
                     $user->fixInfoByPayments();
                 }
-                // 免费用户忽略 
+                // 免费用户忽略
                 if ($user->role_id == 3)
                     continue;
-                if ($user->resetIfExpired())
-                    $this->info("{$user->email} has expired");
+                //modify by chenxin 20171010
+                if ($user->isExpired){
+                    $paymentService->syncSubscriptions([], $user->subscriptions);
+                    $paymentService->syncPayments([], $user->subscriptions);
+                    $user->fixInfoByPayments();
+                    if ($user->resetIfExpired()){
+                        $this->info("{$user->email} has expired");
+                    }
+                }
             }
         }
     }
