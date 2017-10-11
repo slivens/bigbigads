dedaa67694835541771f91d255162fb4cec1939a
rmz <734452449@qq.com>
2017-10-11 17:07:10 +0800

    去掉移动端#

--- resources/assets/js/welcome.js
+++ resources/assets/js/welcome.js
@@ -1,7 +1,7 @@
 $("#continue").click(function() {
     $.get("/record-continue/")
     if (/iphone|nokia|sony|ericsson|mot|samsung|sgh|lg|philips|panasonic|alcatel|lenovo|cldc|midp|wap|mobile/i.test(navigator.userAgent.toLowerCase())) {
-        window.open('/m/#/login', '_self')
+        window.open('/mobile', '_self')
     } else {
         window.open('/app', '_self')
     }
