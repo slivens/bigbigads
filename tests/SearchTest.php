bfcdc5bf6a5574755d3c1b98470fbb963ae04e20
LIUWENCAN <pheye@qq.com>
2017-10-21 21:47:40 +0800

    添加正常搜索与bba:tag的单元测试

--- tests/SearchTest.php
+++ tests/SearchTest.php
@@ -13,7 +13,7 @@ class SearchTest extends TestCase
     public function search($user)
     {
         $faker = app('Faker\Generator');
-        $params = json_decode('{"search_result":"ads","sort":{"field":"last_view_date","order":1},"where":[],"limit":[0,10],"is_why_all":1,"topN":10,"is_stat":0,"keys":[{"string":"adidas","search_fields":"message,name,description,caption,link,adser_username,adser_name,dest_site,buttonlink,event_id","relation":"Must"}],"action":"search"}', true);
+        $params = json_decode('{"search_result":"ads","sort":{"field":"last_view_date","order":1},"where":[],"limit":[0,10],"is_why_all":1,"topN":10,"is_stat":0,"keys":[{"string":"loan","search_fields":"message,name,description,caption,link,adser_username,adser_name,dest_site,buttonlink,event_id","relation":"Must"}],"action":"search"}', true);
         $params['keys'][0]['string'] = $faker->domainWord;
         $response = $this->actingAs($user)
             ->call('POST', '/forward/adsearch', $params);
@@ -27,6 +27,11 @@ class SearchTest extends TestCase
      */
     public function testBasic()
     {
+		/* Cache::shouldReceive('get') */
+                    /* ->once() */
+                    /* ->with('role-standard') */
+                    /* ->andReturn('value'); */
+
         $user = factory(App\User::class)->make(['email' => 'faker@bigbigads.com']);
         if (!($user instanceof User)) {
            $this->assertTrue(false);
@@ -37,8 +42,10 @@ class SearchTest extends TestCase
            $this->assertTrue(false);
            return; 
         }
+		$role->generateCache();
         $user->role_id = $role->id;
         $user->initUsageByRole($user->role);
+        $user->save();
         $this->search($user);
         return $user;
         /* $this->assertTrue(true); */
