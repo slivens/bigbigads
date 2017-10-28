a0858a804f414a9b1561d24fb7e48e04f357f339
yuqinghong <niming175@qq.com>
2017-10-28 19:30:32 +0800

    在profile->account setting中添加addiliate功能（联盟会员推广）；在command.js添加复制到剪贴板的指令

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,9 +79,19 @@ class UserController extends Controller
                 $user->email,
                 'sh7oS9Q3bk0m-Vs3pEeUjCOMKGjoyjf1bVcTfCiY'
             );
-            $res['track'] = $user->affiliate->track;
-            $res['click'] = $user->affiliate->click;
-            $res['action'] = $user->affiliate->action;
+            // 可能存在affiliate不存在的情况
+            if ($user->affiliate) {
+                // 获取http或则是https
+                $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';  
+                $res['user']['track'] = $http_type.$_SERVER['HTTP_HOST']."?track=".$user->affiliate->track;
+                $res['user']['click'] = $user->affiliate->click;
+                $res['user']['action'] = $user->affiliate->action;
+            } else {
+                $res['user']['track'] = false;
+                $res['user']['click'] = 0;
+                $res['user']['action'] = 0;
+            }
+            
         } else {
             $user = AnonymousUser::user($req);
             $res['login'] = false;
--- public/src/pages/common/common.js
+++ public/src/pages/common/common.js
@@ -491,6 +491,47 @@ angular.module('MetronicApp').directive('fancybox', ['$compile', '$timeout', fun
             }
         }
     }])
+
+    /* 
+    * 复制到剪贴板
+    * copy-dom 上有三个属性，分别是value, button-text, success-text
+    * value: 要复制的文本
+    * button-text：按钮上的文字
+    * success-text: 复制成功后的文字
+    */
+    .directive('copyDom', function() {
+        return {
+            restrict: 'EA',
+            link: function(scope, element, attrs) {
+                let poster = $('<div class="copy-dom-wrapper"></div>')
+                let copyText = attrs.value // 要复制的文字
+                let buttonText = attrs.buttonText // 按钮上的文字
+                let successText = attrs.successText // 复制成功后的提示
+                let html = `<input type="text" readOnly = "true" value="${copyText}"/>
+                    <button class="btn btn-primary">
+                        <i class="fa fa-clipboard"></i>
+                        ${buttonText}
+                    </button>
+                    <span></span>`
+                poster.html(html)
+                element.before(poster)
+                element.hide()
+                poster.find('button').click(function() {
+                    let inputDom = poster.find("input")
+                    inputDom.select() // 选择对象
+                    document.execCommand("Copy") // 执行浏览器复制命令
+                    successText && poster.find("span").html("(" + successText + ")").removeClass("hiden-text").addClass("show-text")
+                    // 暂时屏蔽点击按钮
+                    $(this).attr("disabled", "disabled")
+                    // 三秒后隐藏提示语，并恢复按钮可以点击
+                    setTimeout(function() {
+                        poster.find("span").addClass("hiden-text").removeClass("show-text")
+                        poster.find("button").removeAttr("disabled")
+                    }, 3000)
+                })
+            }
+        }
+    })
     // 去重复：定义一个过滤器，用于去除重复的数组，确保显示的每一条都唯一
     .filter('unique', function() {
         return function(collection) {
