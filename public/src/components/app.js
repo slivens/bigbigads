9ba0628d29ad3fe7e08f38f9dfa99c02bc9d77cc
LIUWENCAN <pheye@qq.com>
2017-10-28 17:02:39 +0800

    1. 前台编译优化：每次编译都先清除public/dist 2. 后台语言环境改为通过.env配置 3. WEB APP语言环境实现通过webpack文件配置 4. 尽管angular支持多语言，但为了保持前台与后台前端的一致性 将统一使用i18n-js处理多语言 5. 顶部菜单独立成main-menu的组件 6. 顶部菜单将Advertisement与Bookmark改引用多语言文件，以 演示WEB APP的多语言用法 7. public/src/lib/intl.js是处理WEB APP的核心文件

--- public/src/components/app.js
+++ public/src/components/app.js
@@ -1,11 +1,13 @@
 import './header.js'
 import './ng-spinner-bar.js'
 import template from './app.html'
+import tr from '../lib/intl.js'
 
 /* Setup App Main Controller */
 angular.module('bba.app', ['ui.router', 'bba.header', 'bba.ng-spinner-bar'])
     .controller('AppController', ['$rootScope', function($rootScope) {
         let ctrl = this
+        $rootScope.tr = tr
         $rootScope.$on('$stateChangeSuccess', () => {
             ctrl.serverRendered = true
         })
--- public/src/components/header.js
+++ public/src/components/header.js
@@ -1,8 +1,9 @@
 import './header.scss'
 import template from './header.html'
+import './main-menu.js'
 
 angular
-    .module('bba.header', ['bba.settings'])
+    .module('bba.header', ['bba.settings', 'bba.main-menu'])
     .controller('HeaderController', ['settings', function(settings) {
         var vm = this
         vm.settings = settings
