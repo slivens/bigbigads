225db3b2a4a0021175189f9e91ae805e510425a6
yuqinghong <niming175@qq.com>
2017-10-30 19:17:59 +0800

    修改进入pay的支付路径

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -1,26 +1,26 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bigbigads Pricing | Largest Facebook Ad Examples</title>
     <meta name="description" content="A Facebook ads spy tool to help you find out the competitor marketing strategy, ad creatives, ad insight.">
-    <link rel="shortcut icon" type="image/x-icon" href="./static/images/favicon.ico" media="screen" /> 
+    <link rel="shortcut icon" type="image/x-icon" href="./static/images/favicon.ico" media="screen" />
     <link href="{{bba_version('home.css')}}" rel="stylesheet">
-    <link href="{{bba_version('plan.css')}}" rel="stylesheet">
-    @include('tpl.script')
+    <link href="{{bba_version('plan.css')}}" rel="stylesheet"> @include('tpl.script')
 </head>
+
 <body id="plan">
-@include('tpl.header')
-<section class="plan-banner">
-    <div class="container">
-         <p class="ads-font-36 ads-weight-400 text-center plan-title">What Are You Waiting For?</p>
-         <p class="ads-font-20 ads-weight-400 plan-suptitle text-center">Join thousands of users to know your competitor's profitable marketing strategy now.</p>
-         <div class="plan-card-wrapper text-center">
+    @include('tpl.header')
+    <section class="plan-banner">
+        <p class="ads-font-36 ads-weight-400 text-center plan-title">What Are You Waiting For?</p>
+        <p class="ads-font-20 ads-weight-400 plan-suptitle text-center">Join thousands of users to know your competitor's profitable marketing strategy now.</p>
+        <div class="plan-card-wrapper">
             <div class="plan-card plan-card-blue">
                 <a href="/register" onclick="window.uetq = window.uetq || []; window.uetq.push({'ec':'conversion', 'ea':'click_signup_free','el':'click','ev':12 });ga('send', 'event', 'conversion', 'click', 'signup_free');">
-                    <div class="card-front">
+                    <div class="card-front card-body">
                         <div class="card-header ads-font-30 ads-weight-200">Free</div>
                         <div class="card-content">
                             <div class="ads-font-24 ads-weight-200 card-text">It's Free</div>
@@ -38,13 +38,11 @@
                     </div>
                 </a>
             </div>
-            
             <!--lite-->
             <div class="plan-card plan-card-orange" card-name="lite-card">
                 <div class="card-front card-body" turn-back="lite-card">
                     <div class="card-header ads-font-30 ads-weight-200">Lite <span class="ads-font-12">(BATA)</span></div>
-                    
-                    <div class="card-content" >
+                    <div class="card-content">
                         <div class="ads-font-24 ads-weight-200 card-text">Start from</div>
                         <div class="text-center card-money">
                             <span class="ads-font-16">$</span>
@@ -58,31 +56,31 @@
                         </button>
                     </div>
                 </div>
-                <div class="card-back card-body ">
+                <div class="card-back card-body">
                     <div class="back-header ads-font-22 ads-weight-200 text-center" turn-front="lite-card">
                         7-day money-back
                         <span class="fa fa-chevron-circle-left back-button ads-font-24"></span>
                     </div>
                     <div class="price-list-wrapper">
-                        <a class="price-list" href="/pay?name=Lite_monthly">
-                            <div class="ads-weight-200"> 
-                                <span class="ads-font-24 ">Monthly </span>
-                                (<span class="ads-font-18">$59.99</span><span class="ads-font-14">/m</span>)
+                        <a class="price-list" href="/pay?name=lite_monthly">
+                            <div class="ads-weight-200">
+                                <span class="ads-font-24 ads-weight-200">Monthly </span> (
+                                <span class="ads-font-18">$59.99</span><span class="ads-font-14">/m</span>)
                             </div>
                         </a>
-                        <a class="price-list" href="/pay?name=Lite_quarterly">
-                            <div class="ads-weight-200"> 
-                                <span class="ads-font-24 ads-weight-200">Quarterly</span>
-                                (<span class="ads-font-16">$39.99</span><span class="ads-font-12">/m</span>)
+                        <a class="price-list" href="/pay?name=lite_quarterly">
+                            <div class="ads-weight-200">
+                                <span class="ads-font-24 ads-weight-200">Quarterly</span> (
+                                <span class="ads-font-16">$39.99</span><span class="ads-font-12">/m</span>)
                             </div>
                             <p class="ads-font-12 price-list-text">
                                 Billed as One payment of $119.97
                             </p>
                         </a>
-                        <a class="price-list" href="/pay?name=Lite_annual">
-                            <div class="ads-weight-200"> 
-                                <span class="ads-font-24 ads-weight-200">Annual</span>
-                                (<span class="ads-font-16">$24.99</span><span class="ads-font-12">/m</span>)
+                        <a class="price-list" href="/pay?name=lite_annual">
+                            <div class="ads-weight-200">
+                                <span class="ads-font-24 ads-weight-200">Annual</span> (
+                                <span class="ads-font-16">$24.99</span><span class="ads-font-12">/m</span>)
                             </div>
                             <p class="ads-font-12 price-list-text">
                                 Billed as One payment of $299.88
@@ -91,10 +89,9 @@
                     </div>
                 </div>
             </div>
-            
             <!--standart-->
             <div class="plan-card plan-card-orange" card-name="standard-card">
-                <div class="card-front" turn-back="standard-card" onclick="window.uetq = window.uetq || []; window.uetq.push({'ec':'conversion', 'ea':'click_signup_standard','el':'click','ev':12 });ga('send', 'event', 'conversion', 'click', 'signup_standard');">
+                <div class="card-front card-body" turn-back="standard-card" onclick="window.uetq = window.uetq || []; window.uetq.push({'ec':'conversion', 'ea':'click_signup_standard','el':'click','ev':12 });ga('send', 'event', 'conversion', 'click', 'signup_standard');">
                     <div class="card-header ads-font-30 ads-weight-200">Standard</div>
                     <div class="card-content">
                         <div class="ads-font-24 ads-weight-200 card-text">Start from</div>
@@ -110,31 +107,31 @@
                         </button>
                     </div>
                 </div>
-                <div class="card-back card-body ">
+                <div class="card-back card-body">
                     <div class="back-header ads-font-22 ads-weight-200 text-center" turn-front="standard-card">
                         7-day money-back
                         <span class="fa fa-chevron-circle-left back-button ads-font-24"></span>
                     </div>
                     <div class="price-list-wrapper">
                         <a class="price-list" href="/pay?name=standard_monthly">
-                            <div class="ads-weight-200"> 
-                                <span class="ads-font-24 ">Monthly </span>
-                                (<span class="ads-font-18">$99.00</span><span class="ads-font-14">/m</span>)
+                            <div class="ads-weight-200">
+                                <span class="ads-font-24 ">Monthly </span> (
+                                <span class="ads-font-18">$99.00</span><span class="ads-font-14">/m</span>)
                             </div>
                         </a>
-                        <a class="price-list" href="/pay?name=standard_quarter_monthly">
-                            <div class="ads-weight-200"> 
-                                <span class="ads-font-24 ads-weight-200">Quarterly</span>
-                                (<span class="ads-font-16">$79.00</span><span class="ads-font-12">/m</span>)
+                        <a class="price-list" href="/pay?name=standard_quarterly">
+                            <div class="ads-weight-200">
+                                <span class="ads-font-24 ads-weight-200">Quarterly</span> (
+                                <span class="ads-font-16">$79.00</span><span class="ads-font-12">/m</span>)
                             </div>
                             <p class="ads-font-12 price-list-text">
                                 Billed as One payment of $237.00
                             </p>
                         </a>
-                        <a class="price-list" href="/pay?name=standard">
-                            <div class="ads-weight-200"> 
-                                <span class="ads-font-24 ads-weight-200">Annual</span>
-                                (<span class="ads-font-16">$65.00</span><span class="ads-font-12">/m</span>)
+                        <a class="price-list" href="/pay?name=standard_annual">
+                            <div class="ads-weight-200">
+                                <span class="ads-font-24 ads-weight-200">Annual</span> (
+                                <span class="ads-font-16">$65.00</span><span class="ads-font-12">/m</span>)
                             </div>
                             <p class="ads-font-12 price-list-text">
                                 Billed as One payment of $780.00
@@ -144,73 +141,72 @@
                 </div>
             </div>
         </div>
+    </section>
+    <div class="click-to-down hidden-xs hidden-sm">
+        <a href="#plan-table" id="click-to-down">
+            <div class="down-icon"></div>
+        </a>
     </div>
-</section>
-
-<div class="click-to-down hidden-xs hidden-sm">
-    <a href="#plan-table" id="click-to-down"><div class="down-icon"></div></a>
-</div>
-
-<section class="plan-table" id="plan-table">
-    <div class="container">
-    <div class="row">
-        <div class="col-sm-12 col-md-10 col-md-offset-1 table-responsive">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <td class="ads-font-12 ads-weidth-600 text-center">FEATURES</td>
-                        <td class="ads-font-24 ads-weidth-600 text-center">Free</td>
-                        <td class="ads-font-24 ads-weidth-600 text-center">
-                            Lite
-                            <p class="plan-small">Bata</p>
-                        </td>
-                        <td class="ads-font-24 ads-weidth-600 text-center">Standard</td>
-                        <td class="ads-font-24 ads-weidth-600 text-center">
-                            Plus</p>
-                            <p class="plan-small">coming soon</p>
-                        </td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="table-item-title">
-                        <td>Data Time Interval</td>
-                        <td>3 Months</td>
-                        <td>2 weeks</td>
-                        <td>Daily</td>
-                        <td>Daily</td>
-                    </tr>
-                    <tr>
-                        <td>Search Mode</td>
-                        <td><i class="plan-right"></i></td>
-                        <td><i class="plan-right"></i></td>
-                        <td><i class="plan-right"></i></td>
-                        <td><i class="plan-right"></i></td>
-                    </tr>
-                    <tr>
-                        <td>Sort by Filter</td>
-                        <td><i class="plan-right"></i></td>
-                        <td><i class="plan-right"></i></td>
-                        <td><i class="plan-right"></i></td>
-                        <td><i class="plan-right"></i></td>
-                    </tr>
-                    <tr class="table-item-title">
-                        <td>Call to Action Filter</td>
-                        <td>
-                            <i class="plan-right"></i>
-                        </td>
-                        <td>
-                            <i class="plan-right"></i>
-                        </td>
-                        <td>
-                            <i class="plan-right"></i>
-                        </td>
-                        <td>
-                            <i class="plan-right"></i>
-                        </td>
-                    </tr>
-                    <tr class="table-item-title">
-                        <td>Language Filter
-                            <span class="plan-beta">BETA<span>
+    <section class="plan-table" id="plan-table">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-10 col-md-offset-1 table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <td class="ads-font-12 ads-weidth-600 text-center">FEATURES</td>
+                                <td class="ads-font-24 ads-weidth-600 text-center">Free</td>
+                                <td class="ads-font-24 ads-weidth-600 text-center">
+                                    Lite
+                                    <p class="plan-small">Bata</p>
+                                </td>
+                                <td class="ads-font-24 ads-weidth-600 text-center">Standard</td>
+                                <td class="ads-font-24 ads-weidth-600 text-center">
+                                    Plus</p>
+                                    <p class="plan-small">coming soon</p>
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="table-item-title">
+                                <td>Data Time Interval</td>
+                                <td>3 Months</td>
+                                <td>2 weeks</td>
+                                <td>Daily</td>
+                                <td>Daily</td>
+                            </tr>
+                            <tr>
+                                <td>Search Mode</td>
+                                <td><i class="plan-right"></i></td>
+                                <td><i class="plan-right"></i></td>
+                                <td><i class="plan-right"></i></td>
+                                <td><i class="plan-right"></i></td>
+                            </tr>
+                            <tr>
+                                <td>Sort by Filter</td>
+                                <td><i class="plan-right"></i></td>
+                                <td><i class="plan-right"></i></td>
+                                <td><i class="plan-right"></i></td>
+                                <td><i class="plan-right"></i></td>
+                            </tr>
+                            <tr class="table-item-title">
+                                <td>Call to Action Filter</td>
+                                <td>
+                                    <i class="plan-right"></i>
+                                </td>
+                                <td>
+                                    <i class="plan-right"></i>
+                                </td>
+                                <td>
+                                    <i class="plan-right"></i>
+                                </td>
+                                <td>
+                                    <i class="plan-right"></i>
+                                </td>
+                            </tr>
+                            <tr class="table-item-title">
+                                <td>Language Filter
+                                    <span class="plan-beta">BETA<span>
                         </td>
                         <td>
                             <i class="plan-right"></i>
@@ -492,4 +488,3 @@
     ga('send', 'event', 'conversion', 'page', 'unregister_plan');
 </script>
 </html>
-
