fc81826192690025575a33a4bbe23695b838287f
ChenTeng <shanda030258@hotmail.com>
2017-11-03 22:14:32 +0800

    修改表单提示，增加模型层

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -210,6 +210,21 @@ class InvoiceController extends Controller
                 ],
                 $extraData
             );
+            if (count($user->payments) > 0) {
+                $res = [
+                    'code' => 0,
+                    'desc' => 'The Invoice will be re-generate in few seconds.',
+                    'status' =>'success'
+                ];
+                //推入队列执行,有修改才执行
+                dispatch((new GenerateInvoiceJob(Payment::where('client_id', $user->id)->get(), true, $extraData)));
+            } else {
+                $res = [
+                    'code' => 0,
+                    'desc' => 'Then you must payed for one time.',
+                    'status' =>'success'
+                ];
+            }
         }
         return response()->json($res);
     }
