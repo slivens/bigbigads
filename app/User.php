f241b42b979374759f119e7714ee0f7471a7ef72
CHEN XIN <chenxin@x-grit.com>
2017-11-09 11:12:41 +0800

    修复了 Issue #35,变量名修改

--- app/User.php
+++ app/User.php
@@ -533,20 +533,20 @@ class User extends Authenticatable
 
         // 过期时间设置为有效订单的结束时间+1天，一个时间段内只会有一个有效订单
         // modify by chenxin 20171108, 修复了 Issue #35
-        $now_time = Carbon::now();
-        $longest_expired = $now_time;
+        $nowTime = Carbon::now();
+        $longestExpired = $nowTime;
         foreach ($baseSub->payments as $payment) {
             if ($payment->isEffective()) {
                 $expired = new Carbon($payment->end_date);
                 $expired->addDay();
-                if ($expired->gt($longest_expired)) {
-                    $longest_expired = $expired;
+                if ($expired->gt($nowTime)) {
+                    $longestExpired = $expired;
                 }
             }
         }
-        if ($longest_expired != $now_time && $this->expired != $longest_expired) {
-            Log::info("{$this->email}'s expired is updated:{$this->expired} -> {$longest_expired}");
-            $this->expired = $longest_expired;
+        if ($longestExpired != $nowTime && $this->expired != $longestExpired) {
+            Log::info("{$this->email}'s expired is updated:{$this->expired} -> {$longestExpired}");
+            $this->expired = $longestExpired;
             $dirty = true;
         }
         if ($dirty)
