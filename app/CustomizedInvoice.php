34222b72cd5f74f076cc3f4cb2f764e7a7522006
ChenTeng <shanda030258@hotmail.com>
2017-11-09 20:20:15 +0800

    修改迁移，字段长度限制改为50个字符（原200），修复不同年份但是同月份不能提交自定义内容的Bug

--- app/CustomizedInvoice.php
+++ app/CustomizedInvoice.php
@@ -15,12 +15,13 @@ class CustomizedInvoice extends Model
 
     /**
      * 存储限制，一个月只能存储1次
-     * 若更新时间不在当前月内，则更新
+     * 若更新时间与今天差30天或者以上，则更新
+     * 旧判断会导致不同年的同一个月不能更新
      *
      * @return boolean
      */
     public function canSave()
     {
-        return Carbon::parse($this->updated_at)->month != Carbon::now()->month;
+        return Carbon::parse($this->updated_at)->diffInDays(Carbon::now()) >= 30;// month != Carbon::now()->month;
     }
 }
