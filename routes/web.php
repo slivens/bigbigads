d7c7f947c19e130b48a771ffade2c6c203768d9e
ChenTeng <shanda030258@hotmail.com>
2017-11-13 15:40:53 +0800

    路由修改，按restful api规范修改，由于只提交用户的部分信息，所以用Patch

--- routes/web.php
+++ routes/web.php
@@ -144,10 +144,7 @@ Route::group(['middleware'=>'auth'], function() {
     Route::get('/users/customize_invoice', 'UserController@getInvoiceCustomer');
     Route::post('/users/customize_invoice', 'UserController@setInvoiceCustomer');
     Route::get('/invoices/{invoice_id}', 'InvoiceController@downloadInvoice');
-<<<<<<< HEAD
-    Route::post('users/changeProfile/{type}/{param}', 'UserController@changeProfile');
-=======
->>>>>>> origin/develop
+    Route::patch('users/change_profile', 'UserController@changeProfile');
 });
 
 //pay页面需要支持不登录可访问
