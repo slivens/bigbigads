cef5184de458c72b3cab3697fae2af2ac8d201de
LIUWENCAN <pheye@qq.com>
2017-11-16 00:01:10 +0800

    1. bba:check-uage增加重新生成用户policy缓存的功能 2. 权限视图实现权限、策略的修改（前后端均完成） 3. 权限修改与生成缓存分开操作 4. 解决User显示策略数据时的小BUG 5. 调整Role策略数据显示不一致的提示信息

--- app/Console/Commands/PolicyCommand.php
+++ app/Console/Commands/PolicyCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\User;
 
-class ManagePolicy extends Command
+class PolicyCommand extends Command
 {
     /**
      * The name and signature of the console command.
--- routes/admin.php
+++ routes/admin.php
@@ -8,3 +8,5 @@ Route::get('/refunds/{id}/reject', 'Admin\RefundController@rejectRefund')->name(
 
 Route::get('/permission_map', 'Admin\RoleController@showPermissionMap')->name('permission_map');
 Route::post('/permission_map', 'Admin\RoleController@storePermissionMap')->name('store_permission_map');
+
+Route::post('/permission/cache/generate', 'Admin\RoleController@generatePermissionCache')->name('generate_permission_cache');
