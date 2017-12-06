92e737a191fe3773bbb1c0f3452786b329e92ca2
LIUWENCAN <pheye@qq.com>
2017-12-06 16:01:30 +0800

    bba:upgrade增加后台菜单升级；将升级全部纳入事务

--- app/Console/Commands/UpgradeCommand.php
+++ app/Console/Commands/UpgradeCommand.php
@@ -12,7 +12,7 @@ class UpgradeCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'bba:upgrade {--generate : 生成升级相关的iseed文件} {--recovery : 从iseed文件中升级系统}';
+    protected $signature = 'bba:upgrade {--generate : 生成升级相关的iseed文件} {--recovery : 从iseed文件中升级系统} {--menus : 从seed文件升级菜单}';
 
     /**
      * The console command description.
@@ -46,16 +46,17 @@ class UpgradeCommand extends Command
     {
         if ($this->option('generate')) {
             $this->call('iseed', [
-                'tables' => 'roles,permissions,permission_role,policies,policy_role,policy_user',
+                'tables' => 'roles,permissions,permission_role,policies,policy_role,policy_user,data_types,data_rows,menus,menu_items',
                 '--force' => true
             ]);
         } else if ($this->option('recovery')) {
             // 临时禁止外键以便重建表，高危操作，只有明确自己操作的影响才能使用该功能
-            \Illuminate\Support\Facades\Schema::disableForeignKeyConstraints();
-            $this->seed(\RolesTableSeeder::class);
-            \Illuminate\Support\Facades\Schema::enableForeignKeyConstraints();
             try {
                 DB::beginTransaction();
+                \Illuminate\Support\Facades\Schema::disableForeignKeyConstraints();
+                $this->seed(\RolesTableSeeder::class);
+                \Illuminate\Support\Facades\Schema::enableForeignKeyConstraints();
+
                 $this->call('iseed', [
                     'tables' => 'policy_user',
                     '--force' => true
@@ -71,6 +72,11 @@ class UpgradeCommand extends Command
                 DB::rollBack();
                 $this->comment("something goes wrong, rollback");
             }
+        } else if ($this->option('menus')) {
+            $this->call('iseed', [
+                'tables' => 'data_types,data_rows,menus,menu_items',
+                '--force' => true
+            ]);
         } else {
             $this->error("no operation");
         }
