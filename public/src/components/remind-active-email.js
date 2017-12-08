eed4e93289339e53a935c34ac0079d2e76ca97df
yuqinghong <niming175@qq.com>
2017-12-08 15:48:25 +0800

    关于邮箱验证条件（profile页面显示验证框）加个判断条件（限制free用户）

--- public/src/components/remind-active-email.js
+++ public/src/components/remind-active-email.js
@@ -27,7 +27,13 @@ module.factory('activeEmailReminder', ['$uibModal', 'User', '$window', function(
             })
         },
         check: function() {
-            if (moment(User.info.user.created_at).isBefore('2017-07-31', 'day') && !User.info.user.is_check)
+            /*
+            * 返回false条件
+            *  1）2017.07.31 之前注册的用户
+            *  2）没有验证过的用户 User.info.user.is_check == 0
+            *  3）用户等级是Free
+            */
+            if (moment(User.info.user.created_at).isBefore('2017-07-31', 'day') && !User.info.user.is_check && User.info.user.role.name == 'Free')
                 return false
             return true
         }
