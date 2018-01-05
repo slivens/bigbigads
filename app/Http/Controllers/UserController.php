234d29370615fda7f1a7ca087906b34681821cc8
LIUWENCAN <pheye@qq.com>
2018-01-05 10:04:04 +0800

    修正拼写错误

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -782,7 +782,7 @@ class UserController extends Controller
                 }
             }
             return response()->json(
-                $affAsersArr
+                $affUsersArr
             );
         }
     }
