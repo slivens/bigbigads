3a6068e80691f50eff3873f8adf6366d107249b6
e <734452449@qq.com>
2018-02-11 18:49:00 +0800

    将广告主搜索的提示语都纳入多语言系统中

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -14,4 +14,14 @@ return [
     'bookmark_can_not_modify' => 'Can not modify this bookmark.',
     'service_term' => 'You need agree terms of service',
     'effective_email' => 'check user email effective',
-];
+    'no_adser_search_permisson' => 'you not permission of adser search',
+    'detect_behavior' = 'We detect your ip has abandom behavior',
+    'lack_parameters' => 'Lack of necessary id or sequencing parameters',
+    'reached_search_times' => 'you reached search times today, default result will show',
+    'beyond_result_limit' => 'beyond result limit',
+    'no_permission_adser_analysis' => 'you not permission of adser analysis',
+    'lack_adser_id' => 'lack of adser id',
+    'no_search_permission' => 'no search permission',
+    'Illegal_limit_params' => 'Illegal limit params',
+    'should_sign_in' => 'You should sign in',
+]
